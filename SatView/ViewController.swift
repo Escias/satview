b30b7cde2afcbf422935c94cd7593ba3f4045c4e
Escias <ljimmy98@hotmail.fr>
2022-02-23 10:55:10 +0100

    first commit

---
+++ SatView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SatView
+//
+//  Created by Jimmy Lai on 23/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
