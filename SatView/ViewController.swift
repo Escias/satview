9a98597179d36b73fbd661b6b85f28d297653173
Escias <ljimmy98@hotmail.fr>
2022-02-23 15:57:41 +0100

    init MapKit

--- SatView/ViewController.swift
+++ SatView/ViewController.swift
@@ -6,14 +6,31 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        self.mapView.mapType = MKMapType.hybrid
         // Do any additional setup after loading the view.
     }
 
-
 }
 
+private extension MKMapView {
+  func centerToLocation(
+    _ location: CLLocation,
+    regionRadius: CLLocationDistance = 1000
+  ) {
+    let coordinateRegion = MKCoordinateRegion(
+      center: location.coordinate,
+      latitudinalMeters: regionRadius,
+      longitudinalMeters: regionRadius)
+    setRegion(coordinateRegion, animated: true)
+  }
+}
